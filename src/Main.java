7d4746e8d02a03ba73c55a3d98c6933ab23e3bd1
Kosmas <kosmaspanserraikos@gmail.com>
2024-10-25 23:25:53 +0300

    Testing if commits work from other users

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Testing Commits");
+        System.out.println("123212");
     }
 
 }

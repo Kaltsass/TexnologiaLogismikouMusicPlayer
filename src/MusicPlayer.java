7985c1e95103a95b8d0e30cc94be9fd23177b52e
Kosmas <kosmaspanserraikos@gmail.com>
2024-11-08 11:43:25 +0200

    Fixed

--- src/MusicPlayer.java
+++ src/MusicPlayer.java
@@ -6,186 +6,164 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class MusicPlayer extends PlaybackListener {
-    private Song currentSong;
-    //creating AdvancedPlayer
-    private AdvancedPlayer advancedPlayer;
-    //pauses boolean flag
-    private boolean isPaused;
-    //pausing resuming
-    private int currentFrame;
-    // playlist index
-    private int currentPlaylistIndex;
+    private static final Object playSignal = new Object(); // Για συγχρονισμό σε παύση/συνέχιση
+    private MusicPlayerGUI musicPlayerGUI;  // GUI
+    private Song currentSong;  // Τρέχον τραγούδι
+    private AdvancedPlayer advancedPlayer;  // AdvancedPlayer
+    private boolean isPaused = false;  // Σημαία παύσης
+    private int currentFrame = 0;  // Σημερινό frame αναπαραγωγής
+    private int currentTimeInMilli = 0;  // Χρόνος σε milliseconds
+    private ArrayList<Song> playlist;  // Playlist
+    private int currentPlaylistIndex = 0;  // Δείκτης τραγουδιού στην playlist
 
-    private ArrayList<Song> playlist;
-
-    //constructor
-    public MusicPlayer(){
-
+    // Constructor
+    public MusicPlayer(MusicPlayerGUI musicPlayerGUI) {
+        this.musicPlayerGUI = musicPlayerGUI;
     }
-    public void loadSong(Song song){
-        currentSong = song;
 
-        if(currentSong != null){
+    // Getter και Setter για frame και χρόνο
+    public Song getCurrentSong() {
+        return currentSong;
+    }
+
+    public void setCurrentFrame(int frame) {
+        currentFrame = frame;
+    }
+
+    public void setCurrentTimeInMilli(int timeInMilli) {
+        currentTimeInMilli = timeInMilli;
+    }
+
+    // Φόρτωση ενός τραγουδιού
+    public void loadSong(Song song) {
+        currentSong = song;
+        currentFrame = 0;
+        currentTimeInMilli = 0;
+        if (currentSong != null) {
             playCurrentSong();
         }
     }
 
-    public void loadPlaylist(File playlistFile){
+    // Φόρτωση playlist από αρχείο
+    public void loadPlaylist(File playlistFile) {
         playlist = new ArrayList<>();
-
-        // store the paths from the text file into the playlist array list
-        try{
-            FileReader fileReader = new FileReader(playlistFile);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            // reach each line from the file and store the text int the songPath variable
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(playlistFile))) {
             String songPath;
-            while((songPath = bufferedReader.readLine()) !=null){
-                // create song object based on song path
-                Song song = new Song(songPath);
-
-                // add to playlist array list
-                playlist.add(song);
+            while ((songPath = bufferedReader.readLine()) != null) {
+                playlist.add(new Song(songPath));
             }
-        }catch (Exception e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        if(playlist.size() > 0){
-
-
-            //update current song to the first song in the playlist
-            currentSong = playlist.get(0);
-
-            //start from the beginning frame
-            currentFrame = 0;
-
-
-            //start song
-            playCurrentSong();
+        if (playlist.size() > 0) {
+            currentPlaylistIndex = 0;
+            loadSong(playlist.get(currentPlaylistIndex));
         }
     }
 
-    //flag
-    public void pauseSong(){
-        if(advancedPlayer != null){
-            //updating flag
+    // Παύση τραγουδιού
+    public void pauseSong() {
+        if (advancedPlayer != null && !isPaused) {
             isPaused = true;
-            //stopping the mp3 player
             stopSong();
         }
     }
 
-    public void stopSong(){
-        if(advancedPlayer!=null){
-            advancedPlayer.stop();
-            advancedPlayer.close();
-            advancedPlayer=null;
+    // Διακοπή αναπαραγωγής
+    public void stopSong() {
+        if (advancedPlayer != null) {
+            try {
+                advancedPlayer.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            advancedPlayer = null;
         }
     }
-    public void nextSong(){
-        if (playlist == null)return;
-        // check if we reached the end of the playlist, if so don't do anything
-        if(currentPlaylistIndex + 1 > playlist.size() - 1) return;
 
-        //stop the song
-        stopSong();
-
-        // increase playlist index
-        currentPlaylistIndex++;
-
-        // update current song
-        currentSong = playlist.get(currentPlaylistIndex);
-
-        //reset frame
-        currentFrame = 0 ;
-
-
-
-        playCurrentSong();
-
-    }
-
-    public void prevSong(){
-        if (playlist == null)return;
-
-        // check to see if we can go to previous song
-        if(currentPlaylistIndex - 1 < 0) return;
-
-        //stop the song
-        stopSong();
-
-        // decrease playlist index
-        currentPlaylistIndex--;
-
-        // update current song
-        currentSong = playlist.get(currentPlaylistIndex);
-
-        //reset frame
-        currentFrame = 0 ;
-
-
-        playCurrentSong();
-    }
-
+    // Αναπαραγωγή τρέχοντος τραγουδιού
     public void playCurrentSong() {
-        if(currentSong == null)return;
+        if (currentSong == null) return;
         try {
             FileInputStream fileInputStream = new FileInputStream(currentSong.getFilePath());
             BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
 
-
-            //advanced player
-
             advancedPlayer = new AdvancedPlayer(bufferedInputStream);
             advancedPlayer.setPlayBackListener(this);
 
-
-            //start music
-
             startMusicThread();
+            startPlaybackSliderThread();
 
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-
-    private void    startMusicThread(){
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-
-                    if(isPaused){
-                        //resume music from last frame
-                        advancedPlayer.play(currentFrame,Integer.MAX_VALUE);
-                    }else{
-                        //reset music
-                        advancedPlayer.play();
+    // Νήμα αναπαραγωγής
+    private void startMusicThread() {
+        new Thread(() -> {
+            try {
+                if (isPaused) {
+                    synchronized (playSignal) {
+                        isPaused = false;
+                        playSignal.notify();
                     }
-                }catch (Exception e){
+                    advancedPlayer.play(currentFrame, Integer.MAX_VALUE);
+                } else {
+                    currentFrame = 0;
+                    advancedPlayer.play();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    // Νήμα ενημέρωσης slider
+    private void startPlaybackSliderThread() {
+        new Thread(() -> {
+            while (advancedPlayer != null && !isPaused) {
+                try {
+                    currentTimeInMilli++;
+                    int calculatedFrame = (int) ((double) currentTimeInMilli * 2.08 * currentSong.getFrameRatePerMilliseconds());
+                    musicPlayerGUI.setPlaybackSliderValue(calculatedFrame);
+                    Thread.sleep(10);
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
         }).start();
     }
 
+    // Επόμενο τραγούδι στην playlist
+    public void nextSong() {
+        if (playlist == null || currentPlaylistIndex + 1 >= playlist.size()) return;
+        stopSong();
+        loadSong(playlist.get(++currentPlaylistIndex));
+    }
+
+    // Προηγούμενο τραγούδι στην playlist
+    public void prevSong() {
+        if (playlist == null || currentPlaylistIndex - 1 < 0) return;
+        stopSong();
+        loadSong(playlist.get(--currentPlaylistIndex));
+    }
+
+    // Έναρξη αναπαραγωγής
     @Override
     public void playbackStarted(PlaybackEvent evt) {
-        //called in the beginning of the song
-
         System.out.println("Playback Started");
     }
 
+    // Τέλος αναπαραγωγής
     @Override
     public void playbackFinished(PlaybackEvent evt) {
-        //gets called when the song finishes or if the player gets called
         System.out.println("Playback Finished");
 
-
-        if(isPaused){
-            currentFrame+= (int)((double)evt.getFrame()*currentSong.getFrameRatePerMilliseconds());
-
-        }
+        if (!isPaused) {
+            currentFrame = 0;
+        } else {
+            currentFrame += evt.getFrame();
         }
     }
-
+}
